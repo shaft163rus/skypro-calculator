fa0ca8aa92b59e164931c6a9ad68f9b6055d0521
shaft163rus <shaft163rus@gmail.com>
2023-06-08 22:29:59 +0400

    Добавил @RequestMapping

--- src/main/java/pro/sky/skyprocalculator/controller/MathController.java
+++ src/main/java/pro/sky/skyprocalculator/controller/MathController.java
@@ -1,12 +1,14 @@
 package pro.sky.skyprocalculator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.skyprocalculator.service.BaseMathFunctionService;
 import pro.sky.skyprocalculator.service.BaseMathFunctionServiceImpl;
 
 @RestController
+@RequestMapping("/calculator")
 
 public class MathController {
     private final BaseMathFunctionService mathFunctionService;
@@ -15,22 +17,22 @@ public class MathController {
         this.mathFunctionService = mathFunctionService;
     }
 
-    @GetMapping("/calculator/plus")
+    @GetMapping("/plus")
     public String plus(@RequestParam String num1, @RequestParam String num2) {
         return num1 + " + " + num2 + " = " + mathFunctionService.plus(num1, num2);
     }
 
-    @GetMapping("/calculator/minus")
+    @GetMapping("/minus")
     public String minus(@RequestParam String num1, @RequestParam String num2) {
         return num1 + " - " + num2 + " = " + mathFunctionService.minus(num1, num2);
     }
 
-    @GetMapping("/calculator/multiply")
+    @GetMapping("/multiply")
     public String multiply(@RequestParam("num1") String num1, @RequestParam String num2) {
         return num1 + " * " + num2 + " = " + mathFunctionService.multiply(num1, num2);
     }
 
-    @GetMapping("/calculator/divide")
+    @GetMapping("/divide")
     public String divide(@RequestParam String num1, @RequestParam String num2) {
         return num1 + " / " + num2 + " = " + mathFunctionService.divide(num1, num2);
     }
